e278b721130153b7155c93246243abdd8975f13f
topmonitoring <dodoboy9620@gmail.com>
2019-07-09 21:51:21 +0300

    added responcive form + thankYou page

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,34 +1,61 @@
 import React from 'react';
 import { navigate } from 'gatsby-link';
-import { Section, Container } from '@components/global';
+import { Section } from '@components/global';
+import styled from 'styled-components';
 
-const Order = () => (
-  <Section id="contact" accent>
-    <Container
-      style={{
-        display: 'block',
-        margin: 'auto',
-      }}
-    >
-      <div className="container">
-        <div className="content">
-          <h1>Contact</h1>
-          <form
-            name="contact"
-            method="post"
-            action="/contact/thanks/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            onSubmit={handleSubmit}
-          >
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isValidated: false };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => alert(error));
+  };
+
+  render() {
+    return (
+      <Section id="contact" accent>
+        <form
+          name="contact"
+          method="post"
+          action="/ThankYou/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <FormGrid>
+            <h1 style={{ gridArea: 'title' }}>Contact</h1>
             {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
             <input type="hidden" name="form-name" value="contact" />
             <div hidden>
               <label>
                 Don’t fill this out:{' '}
-                <input name="bot-field" onChange={handleChange} />
+                <input name="bot-field" onChange={this.handleChange} />
               </label>
             </div>
+
             <div className="field">
               <label className="label" htmlFor={'name'}>
                 Your name
@@ -38,28 +65,37 @@ const Order = () => (
                   className="input"
                   type={'text'}
                   name={'name'}
-                  onChange={handleChange}
+                  onChange={this.handleChange}
                   id={'name'}
                   required={true}
+                  style={{ gridArea: 'name', width: '100%' }}
                 />
               </div>
             </div>
+
             <div className="field">
               <label className="label" htmlFor={'email'}>
-                Email
+                Your e-mail
               </label>
               <div className="control">
                 <input
                   className="input"
                   type={'email'}
                   name={'email'}
-                  onChange={handleChange}
+                  onChange={this.handleChange}
                   id={'email'}
                   required={true}
+                  style={{ gridArea: 'email', width: '100%' }}
                 />
               </div>
             </div>
-            <div className="field">
+
+            <div
+              className="field"
+              style={{
+                gridArea: 'message',
+              }}
+            >
               <label className="label" htmlFor={'message'}>
                 Message
               </label>
@@ -67,47 +103,39 @@ const Order = () => (
                 <textarea
                   className="textarea"
                   name={'message'}
-                  onChange={handleChange}
+                  onChange={this.handleChange}
                   id={'message'}
                   required={true}
+                  style={{
+                    width: '100%',
+                    height: '150px',
+                  }}
                 />
               </div>
             </div>
-            <div className="field">
+            <div className="field" style={{ gridArea: 'button' }}>
               <button className="button is-link" type="submit">
                 Send
               </button>
             </div>
-          </form>
-        </div>
-      </div>
-    </Container>
-  </Section>
-);
-
-const handleChange = e => {
-  this.setState({ [e.target.name]: e.target.value });
-};
-
-const handleSubmit = e => {
-  e.preventDefault();
-  const form = e.target;
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encode({
-      'form-name': form.getAttribute('name'),
-      ...this.state,
-    }),
-  })
-    .then(() => navigate(form.getAttribute('action')))
-    .catch(error => alert(error));
-};
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
+          </FormGrid>
+        </form>
+      </Section>
+    );
+  }
 }
+const FormGrid = styled.div`
+  text-align: center;
+  margin: auto;
+  width: 40%;
+  padding: 10px;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr 1fr;
 
-export default Order;
+  grid-template-areas:
+    'title title'
+    'name email'
+    'message message'
+    'button button';
+`;
---
+++ src/pages/ThankYou.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '@common/Layout';
+import Footer from '@sections/Footer';
+import styled from 'styled-components';
+
+const ThankYouPage = () => (
+  <Layout>
+    <Center>
+      <h1>Thank you!</h1>
+      <p>This is a custom thank you page for form submissions</p>
+
+      <Link to="/">Go back to the homepage</Link>
+    </Center>
+
+    <Footer />
+  </Layout>
+);
+
+export default ThankYouPage;
+
+const Center = styled.div`
+  text-align: center;
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+`;
